52348bbf00e6167dd208fe8502ca0dfd6fdbeead
emin <mustafazadeemin2000@gmail.com>
2022-07-06 13:17:10 +0400

    product comments section

--- src/pages/Product/components/Comments.tsx
+++ src/pages/Product/components/Comments.tsx
@@ -2,9 +2,19 @@ import React, { FC } from "react";
 import { Link, useParams } from "react-router-dom";
 import {
   CommentsBottom,
+  CommentsComment,
+  CommentsCommentContainer,
   CommentsContainer,
+  CommentsStar,
   CommentsTop,
+  FormContainer,
 } from "./styles/Comments.styled";
+
+/* Images */
+import star_full from "../../../assets/images/icons/star_full.png";
+import star from "../../../assets/images/icons/star.png";
+import { Container } from "../../ReusuableComponents/styles/Container.styled";
+import Button from "../../ReusuableComponents/Button";
 
 const Comments: FC = () => {
   const { id } = useParams();
@@ -17,7 +27,86 @@ const Comments: FC = () => {
           <Link to={`/product/comments/${id}`}>Rəylər</Link>
         </div>
       </CommentsTop>
-      <CommentsBottom></CommentsBottom>
+      <CommentsBottom>
+        <CommentsCommentContainer>
+          <CommentsStar>
+            <h2>4</h2>
+            <div>
+              <img src={star_full} alt="star" />
+              <img src={star_full} alt="star" />
+              <img src={star_full} alt="star" />
+              <img src={star_full} alt="star" />
+              <img src={star} alt="star" />
+            </div>
+          </CommentsStar>
+          <CommentsComment>
+            <div>
+              <h2>Gunel Mammadova</h2>
+              <p>5 gün əvvəl</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              mattis viverra lacus ut et, etiam. Vel ut in nunc nunc ut sit nibh
+              tortor sit. Consectetur sit auctor odio quis suspendisse tincidunt
+              quis. Et tristique amet aenean nibh porttitor quis aliquam
+              integer. Consectetur lacus, volutpat malesuada libero.
+              Sollicitudin libero pharetra a.
+            </p>
+          </CommentsComment>
+        </CommentsCommentContainer>
+        <CommentsCommentContainer>
+          <CommentsStar>
+            <h2>4</h2>
+            <div>
+              <img src={star_full} alt="star" />
+              <img src={star_full} alt="star" />
+              <img src={star_full} alt="star" />
+              <img src={star_full} alt="star" />
+              <img src={star} alt="star" />
+            </div>
+          </CommentsStar>
+          <CommentsComment>
+            <div>
+              <h2>Gunel Mammadova</h2>
+              <p>5 gün əvvəl</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi
+              mattis viverra lacus ut et, etiam. Vel ut in nunc nunc ut sit nibh
+              tortor sit. Consectetur sit auctor odio quis suspendisse tincidunt
+              quis. Et tristique amet aenean nibh porttitor quis aliquam
+              integer. Consectetur lacus, volutpat malesuada libero.
+              Sollicitudin libero pharetra a.
+            </p>
+          </CommentsComment>
+        </CommentsCommentContainer>
+        <FormContainer>
+          <Container>
+            <h2>Rəy Bildir</h2>
+            <div>
+              <div className="form-group">
+                <label htmlFor="">Ad</label>
+                <input type="text" placeholder="Adınızı daxil edin" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="">Soyad</label>
+                <input type="text" placeholder="Soyadınızı daxil edin" />
+              </div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Rəy bildirdiyiniz məhsulu seçin</label>
+              <select id="">
+                <option value="1">Məhsulu seçin</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <label htmlFor="">Rəyinizi yazın</label>
+              <textarea placeholder="Rətinizi buraya yazın"></textarea>
+            </div>
+            <Button title="Rəyini bildir" bg="#2DD06E" color="#ffffff" />
+          </Container>
+        </FormContainer>
+      </CommentsBottom>
     </CommentsContainer>
   );
 };
--- src/pages/Product/components/styles/Comments.styled.tsx
+++ src/pages/Product/components/styles/Comments.styled.tsx
@@ -38,4 +38,155 @@ export const CommentsTop = styled.div`
 
 export const CommentsBottom = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+export const CommentsCommentContainer = styled.div`
+  display: flex;
+  padding: 40px 0;
+  border-bottom: 1px solid #bdbdbd;
+
+  @media (max-width: 650px) {
+    flex-direction: column;
+    align-items: center;
+  }
+`;
+
+export const CommentsStar = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  flex: 1;
+  border-right: 1px solid #bdbdbd;
+
+  h2 {
+    font-weight: 300;
+    font-size: 72px;
+    line-height: 91px;
+    color: #000000;
+    margin-bottom: 40px;
+  }
+
+  @media (max-width: 650px) {
+    margin-bottom: 20px;
+    border-right: none;
+    border-bottom: 1px solid #bdbdbd;
+    padding-bottom: 20px;
+  }
+`;
+
+export const CommentsComment = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 4;
+  padding: 0 40px;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  h2 {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 28px;
+    color: #4f4f4f;
+    margin-bottom: 16px;
+  }
+
+  p {
+    font-size: 16px;
+    line-height: 24px;
+    color: #4f4f4f;
+  }
+
+  @media (max-width: 650px) {
+    padding: 0;
+
+    div {
+      justify-content: center;
+
+      p {
+        display: none;
+      }
+    }
+  }
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 40px 0;
+  padding-bottom: 150px;
+
+  h2 {
+    font-weight: 600;
+    font-size: 20px;
+    line-height: 28px;
+    color: #000000;
+    text-align: center;
+  }
+
+  > div {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+  }
+
+  > div > div:nth-child(2) {
+    display: flex;
+    gap: 30px;
+  }
+
+  .form-group {
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+
+    label {
+      font-size: 16px;
+      line-height: 22px;
+      color: #4f4f4f;
+      margin-bottom: 8px;
+    }
+
+    input,
+    select,
+    textarea {
+      background: #f2f2f2;
+      border-radius: 8px;
+      outline: none;
+      border: none;
+      height: 50px;
+      width: 100%;
+      padding: 10px;
+      font-size: 15px;
+    }
+
+    textarea {
+      width: 100%;
+      height: 144px;
+    }
+  }
+
+  button {
+    align-self: flex-end;
+    transition: all 0.3s;
+    cursor: pointer;
+
+    &:hover {
+      background: #333333 !important;
+    }
+  }
+
+  @media (max-width: 650px) {
+    > div > div:nth-child(2) {
+      flex-direction: column;
+    }
+
+    button {
+      width: 100%;
+    }
+  }
 `;
