bf0a8d5ff47a1951ed9df59adf7beb77bfc154c7
emin <mustafazadeemin2000@gmail.com>
2022-12-04 21:45:22 +0400

    changing backend url

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const token = localStorage.getItem("jwt");
 export default axios.create({
-  baseURL: "https://rocky-cove-19186.herokuapp.com/api/v1/",
+  baseURL: "https://tello-back-4q57qwduw-emin-m.vercel.app/api/v1/",
   headers: {
     authorization: token ? "Bearer " + token : "",
     Accept: "application/json",
